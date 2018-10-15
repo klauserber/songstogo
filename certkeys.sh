8a1a3851fbc5ebe83101bc5440f61a34940d6e23
Klaus Erber <k.erber@isium.de>
2018-10-15 10:48:57 +0200

    fix google android login

--- certkeys.sh
+++ certkeys.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 
+# https://coderwall.com/p/r09hoq/android-generate-release-debug-keystores
+
 # debug
+keytool -genkey -v -keystore debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000
+
+
 keytool -list -v -keystore ~/.android/debug.keystore -alias androiddebugkey -storepass android -keypass android
 
 keytool -exportcert -alias androiddebugkey -keystore %HOMEPATH%\.android\debug.keystore | openssl sha1 -binary | openssl base64
