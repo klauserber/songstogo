3949d69f467f6efc7e9dac3c2f15ef2e5c57750c
Klaus Erber <k.erber@isium.de>
2018-10-15 17:42:03 +0200

    firebase hosting

--- certkeys.sh
+++ certkeys.sh
@@ -6,9 +6,11 @@
 # debug
 keytool -genkey -v -keystore debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000
 
-
+# google: https://console.firebase.google.com/u/0/project/de-isium-songstogo/settings/general/android:de.isium.songstogo
 keytool -list -v -keystore ~/.android/debug.keystore -alias androiddebugkey -storepass android -keypass android
 
+# facebook: https://developers.facebook.com/apps/238695393326428/dashboard/
+# Issue: https://github.com/jeduan/cordova-plugin-facebook4/issues/599
 keytool -exportcert -alias androiddebugkey -keystore %HOMEPATH%\.android\debug.keystore | openssl sha1 -binary | openssl base64
 
 #release
