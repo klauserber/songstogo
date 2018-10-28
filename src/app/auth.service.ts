6027f05ad6e6546eec500e906e9035729a191a2b
Klaus Erber <ke@Kenobi.lan.isium.de>
2018-10-28 22:34:23 +0100

    icons & fixes

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -38,8 +38,8 @@ export class AuthService {
       if(this.platform.is("cordova")) {
         this.google.login(    {
           //'scopes': '... ', // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
-          //'webClientId': '1031297615094-eo5chp8lq9phq2aq48gn9r2vmnsblrha.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
-          'webClientId': '1031297615094-ll0soqmrbnkq6ft66plp7h706oid3t5k.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+          'webClientId': '1031297615094-eo5chp8lq9phq2aq48gn9r2vmnsblrha.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+          //'webClientId': '1031297615094-ll0soqmrbnkq6ft66plp7h706oid3t5k.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
           
           'offline': true, // optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
         }).then((obj) => {
