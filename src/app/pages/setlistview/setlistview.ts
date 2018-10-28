46cd40502e4c8ddca8f8b8d8bafdb631695bf391
Klaus Erber <ke@Kenobi.lan.isium.de>
2018-10-28 16:46:20 +0100

    redesign setlistview, to fix the slider freeze bug

--- src/app/pages/setlistview/setlistview.ts
+++ src/app/pages/setlistview/setlistview.ts
@@ -1,12 +1,12 @@
 import { FeedbackController } from './../../feedback.controller';
-import { NavController, AlertController } from '@ionic/angular';
+import { NavController, AlertController, Slides, Content } from '@ionic/angular';
 import { switchMap } from 'rxjs/operators';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { SetListSliderPage } from './../setlistslider/setlistslider';
 import { SetListService, SetListEntryModel } from './../../setlist.service';
 import { SetListeditPage } from './../setlistedit/setlistedit';
 import { DataService, SetList } from './../../data.service';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 import { AuthService } from '../../auth.service';
 
@@ -14,33 +14,86 @@ import { AuthService } from '../../auth.service';
 @Component({
   selector: 'page-setListview',
   templateUrl: 'setlistview.html',
+  styleUrls: [ "setlistview.scss" ]
 })
 export class SetListviewPage implements OnInit {
+  
+  @ViewChild(Content) content: Content;
+  @ViewChild(Slides) slides: Slides;
+
+
+  listMode = true;
 
   setList: SetList;
   entries: SetListEntryModel[];
+
+  showPrev: boolean = true;
+  showNext: boolean = true;
   
+  constructor(private authService: AuthService, private route: ActivatedRoute, private dataService: DataService, private setListService: SetListService,
+    private alertCtrl: AlertController, private feedbackCtrl: FeedbackController, private navController: NavController) {}
 
   ngOnInit(): void {
     this.initSetList();
     this.authService.loginState.subscribe((user) => this.initSetList());
   }
 
-  constructor(private authService: AuthService, private route: ActivatedRoute, private dataService: DataService, private setListService: SetListService,
-    private alertCtrl: AlertController, private feedbackCtrl: FeedbackController, private navController: NavController) {}
+  ngAfterContentChecked() {
+    this.slides.ionSlideDidChange.subscribe((val) => {
+      this.updateButtons();
+    });  
+  }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SetListviewPage');
+  goBack(event) {
+    if(this.isSlideMode()) {
+      this.listMode = true;
+      event.stopPropagation();
+    }
+    else {
+      this.navController.goBack();
+    }
+  }
+
+  isListMode() {
+    return this.listMode;
+  }
+
+  isSlideMode() {
+    return !this.listMode;
+  }
+
+  isShowPrev() {
+    return !this.listMode && this.showPrev;
+  }
+  isShowNext() {
+    return !this.listMode && this.showNext;
+  }
+
+  openSlide(idx: number) {
+    this.listMode = false;
+    setTimeout(() => {
+      this.slides.update();
+      this.slides.length().then((val) => {
+        console.log("slides length #2: " + val)
+        this.slides.slideTo(idx, 0);
+        this.content.scrollToTop();
+        this.updateButtons();
+      });
+    }, 300);
+  }
+
+  private updateButtons() {
+    this.slides.isBeginning().then((val) => this.showPrev = !val);
+    this.slides.isEnd().then((val) => this.showNext = !val);
   }
 
   async initSetList() {
     this.route.paramMap.pipe(switchMap((params: ParamMap) => this.dataService.findSetListById(params.get("id"))))
       .subscribe((setList) => {
         this.setList = setList;
-        this.entries = this.setListService.createEntriesModel(setList.setListEntries);
+        this.entries = this.setListService.createEntriesModel(setList.setListEntries);        
       });    
   }
-
 
   async showRemoveConfirm(event) {
     event.stopPropagation();
